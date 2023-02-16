01d8ff44742bf4c17695370696568213177875ff
Nabil ork <99547322+Nabil-ork@users.noreply.github.com>
2023-02-16 19:47:20 +0100

    Update supprimer.php

--- supprimer.php
+++ supprimer.php
@@ -1,9 +1,12 @@
 <?php
   session_start();
+  //récupération de l'id dans le lien
   $id= $_GET['id'];
-  if ($_SESSION['logedin']==true){            
-    require "cnx.php";   
-    try {         
+  if ($_SESSION['logedin']==true){ 
+    //connexion a la base de données
+    require_once "cnx.php";   
+    try {
+        //requête de suppression
         $operation = $connexion->prepare( "DELETE FROM emloyee WHERE ID = $id");
         $operation->execute();
         $user = $operation->fetchAll(PDO::FETCH_OBJ);
@@ -12,6 +15,7 @@
         echo $e->getMessage();
     }
   }else{
+    //redirection vers la page Login.php
     header("Location:Login.php");
   }
 ?>
